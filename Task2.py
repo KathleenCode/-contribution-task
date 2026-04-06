5b88a23ebfde72019a4e1b9216829191b0e44fe7
KathleenCode <kathleenbonaventure@gmail.com>
2026-04-06 17:44:45 +0000

    fixed

--- Task2.py
+++ Task2.py
@@ -1,48 +1,24 @@
-# import pandas as pd
-# import requests
+import pandas as pd
+import requests
 
-# currentfile = pd.read_csv("Task 2 - Intern.csv", delimiter = " ")
+currentfile = pd.read_csv("Task 2 - Intern.csv", names = [ ], delimiter = " ")
 
-# f = open("Task 2 - Intern.csv")
-# f = f.read()
+# print(currentfile.index[0])
 
-# print(f[0:7])
-
-# currentfile = currentfile[0:]
+currentfile = currentfile.index[1:]
 
 # print(currentfile)
 
-# print(f)
 
-# def loopthroughurl(aurl):
-#     url = str(aurl).strip()
-        
-#     if not url.startswith(('http://', 'https://')):
-#         url = 'http://' + url
-#     response = requests.get(aurl)
-#     print(response.status_code)
-#     return response.status_code
-
-# qaa = map(loopthroughurl, f)
-
-# for singurl in qaa:
-#     print(singurl)
-#     print(f'(singurl.status_code) singurl')
-
-import requests
-
-
-f = open("Task 2 - Intern.csv")
-f = f.read()
-urls = [line.strip() for line in f if line.strip()]
-
-print(f"Loaded {len(urls)} URLs")
-
-for url in urls:
+def loopthroughurl(aurl):
     try:
-        if not url.startswith(('http://', 'https://')):
-            url = "https://" + url
-        response = requests.get(url, timeout=10)
-        print(f"{response.status_code} {url}")
+        response = requests.get(aurl)
+        # print(response.status_code)
+        return response.status_code, aurl
     except Exception as e:
-        print(f"Error with {url}: {e}")
+        print(str(e))
+
+qaa = map(loopthroughurl, currentfile)
+
+for singurl, au in qaa:
+    print(f'({singurl}) {au}')
