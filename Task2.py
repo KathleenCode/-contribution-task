ec7bb33186980cb14d41ee20de43533725e126a8
KathleenCode <kathleenbonaventure@gmail.com>
2026-04-12 18:40:48 +0000

    m

--- Task2.py
+++ Task2.py
@@ -2,17 +2,15 @@ import pandas as pd
 import requests
 
 currentfile = pd.read_csv("Task 2 - Intern.csv", names = [ ], delimiter = " ")
-
 currentfile = currentfile.index[1:]
 
 def loopthroughurl(aurl):
     try:
+        aurl = str(aurl)
         response = requests.get(aurl)
-        # print(response.status_code)
-        print((response.status_code), aurl)
-        # return response.status_code, aurl
+        print(f"({response.status_code}), {aurl}")
     except Exception as e:
-        print((str(e)), aurl)    
+        print(e, aurl)       
 
 qaa = map(loopthroughurl, currentfile)
 
